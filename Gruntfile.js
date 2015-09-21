3e9c7a7d758a8cb8f319dc6548b2e4c43a413773
Lauren Barker Macbook Air <lauren@Mathtv.com>
2015-09-21 01:12:58 -0700

    updated nav isnt getting pulled in during page transition.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,9 +23,61 @@ module.exports = function(grunt) {
                 //compiling frontend.less into frontend.css
                 "./assets/css/style.css":"./assets/scss/style.scss",
                 "./assets/css/nav.css":"./assets/scss/nav.scss",
+                "./assets/css/page_transition.css":"./assets/scss/page_transition.scss",
               }
           }
       },
+
+      concat: {
+        options: {
+          separator: '\n',
+        }, // Future development, find way to dynamically conact files without listing them here ...
+        dist_js: {
+          src: [
+            'assets/plugins/jquery.min.js',
+            'assets/plugins/bootstrap/js/bootstrap.min.js',
+            'assets/plugins/jquery-inview/jquery.inview.min.js',
+            'assets/plugins/jquery-easing/jquery.easing.min.js',
+            'assets/js/main.js',
+            'assets/js/animation.js',
+            'assets/js/component/bar-chart.js',
+            'assets/js/component/animation.js'
+          ],
+          dest: 'dist/js/base.js',
+        },
+        dist_css: {
+          src: [
+            'assets/plugins/bootstrap/css/bootstrap.min.css',
+            'assets/plugins/font-awesome/css/font-awesome.min.css',
+            'assets/plugins/animate-css/animate.min.css',
+            'assets/css/style.css',
+            'assets/css/nav.css',
+            'assets/css/page_transition.css'
+          ],
+          dest: 'dist/css/style.css',
+        }
+      },
+
+        // Uglify task info. Compress the js files.
+      uglify: {
+        options: {
+          mangle: true,
+          preserveComments: 'some'
+        },
+        my_target: {
+          files: {
+            'dist/js/base.min.js': ['dist/js/base.js',],  
+          }
+        }
+      },
+
+      cssmin: {
+        combine: {
+          files: {
+            'dist/css/style.min.css': ['dist/css/style.css'],
+          }
+        }
+      }
       
     });
 
@@ -35,4 +87,6 @@ module.exports = function(grunt) {
     // Plugin loading
     grunt.registerTask('default',   []);
     grunt.registerTask('buildcss',  ['sass']);
+    grunt.registerTask('deploy',  ['sass','concat','uglify','cssmin']);
+
 };
---
+++ includes/nav.php
@@ -0,0 +1,82 @@
+<a href="#cd-nav" class="cd-nav-trigger">Menu 
+        <span class="cd-nav-icon"></span>
+
+        <svg x="0px" y="0px" width="54px" height="54px" viewBox="0 0 54 54">
+            <circle fill="transparent" stroke="#656e79" stroke-width="1" cx="27" cy="27" r="25" stroke-dasharray="157 157" stroke-dashoffset="157"></circle>
+        </svg>
+    </a>
+
+    <div id="cd-nav" class="cd-nav">
+        <div class="cd-navigation-wrapper">
+            <div class="cd-half-block">
+                <h2>Menu</h2>
+
+                <nav>
+                    <ul class="cd-primary-nav list-unstyled">
+                        <li><a href="#intro" class="selected">Welcome</a></li>
+                        <li><a href="projects.php" data-type="page-transition" class="cd-btn full">Works</a></li>
+                        <li><a href="#timeline">About</a></li>
+                        <li><a href="hireme.html">Hire Me!</a></li>
+                    </ul>
+                   <ul class="cd-primary-nav">
+                        <li><a href="index.html" class="selected" data-type="page-transition">Welcome</a></li>
+                        <li><a href="project1.html" data-type="page-transition">Works</a></li>
+                        
+                    </ul>
+                </nav>
+            </div><!-- .cd-half-block -->
+            
+            <div class="cd-half-block">
+                <h2>Contacting Me</h2>
+                <div class="row">
+
+                        <div class="col-xs-4">
+                            <div class="social-icon-item text-center not-right-column top-column">
+                                <a href="https://www.linkedin.com/in/weblbdesigns">
+                                    <span class="linkedin-icon">
+                                        <i class="fa fa-linkedin fa-5x"></i>
+                                    </span>
+
+                                    <h4>LinkedIn</h4>
+                                </a>
+                            </div>
+                            <!-- //.social-icon-item -->
+                        </div>
+                        <!-- //.col-xs-6 col-sm-4 col-md-2 -->
+
+                        <div class="col-xs-4">
+                            <div class="social-icon-item text-center not-right-column top-column">
+                                <a href="https://github.com/lbarker">
+                                    <span class="github-icon">
+                                        <i class="fa fa-github fa-5x"></i>
+                                    </span>
+
+                                    <h4>GitHub</h4>
+                                </a>
+                            </div>
+                            <!-- //.social-icon-item -->
+                        </div>
+                        <!-- //.col-xs-6 col-sm-4 col-md-2 -->
+                        
+                        <div class="col-xs-4">
+                            <div class="social-icon-item text-center not-right-column top-column">
+                                <a href="https://plus.google.com/u/0/113805534253450447736/about/p/pub">
+                                    <span class="social-icon">
+                                        <i class="fa fa-google-plus fa-5x"></i>
+                                    </span>
+
+                                    <h4>Google +</h4>
+                                </a>
+                            </div>
+                            <!-- //.social-icon-item -->
+                        </div>
+                        <!-- //.col-xs-6 col-sm-4 col-md-2 -->
+
+                       
+                </div>
+                <img src="/assets/img/about/cayucos_2.jpg" class="img-thumbnail">
+                
+            </div> <!-- .cd-half-block -->
+        </div> <!-- .cd-navigation-wrapper -->
+    </div> <!-- .cd-nav -->   
+    
